66f10aa9d3ee4192e0604c7eb186ff00e67993cb
Ar2r <athomicus@gmail.com>
2025-05-11 13:21:51 +0200

    stars paralax

---
+++ main/stars1_line.go
@@ -0,0 +1,76 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"background_1\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 256.0\n"
+  "  y: 224.0\n"
+  "}\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/background.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 235.0
+    y: -10.0
+  }
+}
+embedded_components {
+  id: "sprite1"
+  type: "sprite"
+  data: "default_animation: \"background_1\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 256.0\n"
+  "  y: 224.0\n"
+  "}\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/background.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: -20.0
+    y: -10.0
+  }
+}
+embedded_components {
+  id: "sprite2"
+  type: "sprite"
+  data: "default_animation: \"background_1\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 256.0\n"
+  "  y: 224.0\n"
+  "}\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/background.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 737.0
+    y: -10.0
+  }
+}
+embedded_components {
+  id: "sprite3"
+  type: "sprite"
+  data: "default_animation: \"background_1\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 256.0\n"
+  "  y: 224.0\n"
+  "}\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/background.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 482.0
+    y: -10.0
+  }
+}
---
+++ main/stars3_line.go
@@ -0,0 +1,72 @@
+embedded_components {
+  id: "stars3_2"
+  type: "sprite"
+  data: "default_animation: \"background_4\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 256.0\n"
+  "  y: 224.0\n"
+  "}\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/background.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 242.0
+  }
+}
+embedded_components {
+  id: "stars3_3"
+  type: "sprite"
+  data: "default_animation: \"background_4\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 256.0\n"
+  "  y: 224.0\n"
+  "}\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/background.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 485.0
+  }
+}
+embedded_components {
+  id: "stars3_4"
+  type: "sprite"
+  data: "default_animation: \"background_4\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 256.0\n"
+  "  y: 224.0\n"
+  "}\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/background.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 733.0
+  }
+}
+embedded_components {
+  id: "stars3_1"
+  type: "sprite"
+  data: "default_animation: \"background_4\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 256.0\n"
+  "  y: 224.0\n"
+  "}\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/background.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: -10.0
+  }
+}
