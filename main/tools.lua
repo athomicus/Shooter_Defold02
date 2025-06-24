d514034c4b809aed02171544e0da5a0de8275313
athomicus <athomicus@gmail.com>
2025-06-24 17:00:28 +0200

    fixes

--- main/tools.lua
+++ main/tools.lua
@@ -1,15 +1,19 @@
 local T={}
 function T.remove_object_from_table(tbl, object)
-    for i, v in ipairs(tbl) do
-        if v == object then
-            table.remove(tbl, i)
-            break
-        end
+  for i, v in ipairs(tbl) do
+    if v == object then
+      table.remove(tbl, i)
+      break
     end
+  end
 end
 
- function T.is_empty(list)
-    return next(list) == nil -- Returns true if the list is empty, false otherwise
+function T.count_elements(list)
+  return #list
+end
+
+function T.is_empty(list)
+  return next(list) == nil -- Returns true if the list is empty, false otherwise
 end
 
 return T
